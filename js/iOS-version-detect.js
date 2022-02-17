12b62551fe1b5f594486a1dc9a651eae75382999
JimmyXianJiChen <68801742+JimmyXianJiChen@users.noreply.github.com>
2022-02-17 19:19:49 +0800

    fix: no alert in Chrome in iOS

--- js/iOS-version-detect.js
+++ js/iOS-version-detect.js
@@ -157,10 +157,37 @@ function detect() {
   const info = {
     info_os: BrowserDetect.OS,
     info_browser_version: BrowserDetect.version,
+    BrowserDetect
   };
-	if(info["info_os"]=="iPhone/iPod" && parseFloat(info["info_browser_version"])<=15.1) {
+  if(info["info_os"]=="iPhone/iPod"){
+	if(info["info_browser_version"]=="an unknown version"){
+		var re = /[0-9_]+(?= like Mac OS)/;
+		let version_str = re.exec(info["BrowserDetect"]["dataBrowser"][0]["string"])[0].replaceAll("_",".");
+		var version;
+		var cnt;
+		if(version_str.split(".").length-1>=2){//if the version string contains more than one dot, we will only take the numbers before the second dot to parse as float for comparsion
+			for(let i=0, cnt=0; i<version_str.length; i++){
+				if(version_str[i]=="."){
+					if(cnt==0){
+						cnt=1;
+					}
+					else if(cnt==1){
+						version = parseFloat(version_str.substring(0,i));
+						break;
+					}
+				}	
+			}
+		}
+		else{
+			version = parseFloat(version_str);
+		}
+		info["info_browser_version"] = version;
+	}
+
+	if(parseFloat(info["info_browser_version"])<=15.1){
 		alert("Please update your iOS version to no older than iOS 15.1!");
 	}
+   }
   return info;
 }
 const detectDevice = detect();
